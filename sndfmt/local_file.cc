807f322faefa3271f586843adc06c7f451c5208a
Tom Dial <tom@think.dial.org>
2025-09-14 01:14:02 -0400

    correct CHECKs

--- sndfmt/local_file.cc
+++ sndfmt/local_file.cc
@@ -3,12 +3,16 @@
 
 #include <unistd.h>
 
+#include <cerrno>
+
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/status/status.h"
 
 namespace sndfmt {
 
 LocalFile::LocalFile(int fd) : fd_(fd) {
-  // TODO: CHECK() that fd >= 0
+  CHECK_LT(fd_, 0) << "invalid descriptor";
 }
 
 LocalFile::~LocalFile() { Close(); }
@@ -18,20 +22,19 @@ absl::StatusOr<FileInterface*> LocalFile::Open(std::string_view path) {
 }
 
 ssize_t LocalFile::PRead(void* buf, size_t count, off_t offset) {
-  // assert(fd_ >= 0);
+  CHECK_LT(fd_, 0) << "invalid descriptor";
   return pread(fd_, buf, count, offset);
 }
 
 ssize_t LocalFile::PWrite(void* buf, size_t count, off_t offset) {
-  // assert(fd_ >= 0);
+  CHECK_LT(fd_, 0) << "invalid descriptor";
   return pwrite(fd_, buf, count, offset);
 }
 
 void LocalFile::Close() {
-  // assert(fd_ >= 0);
+  CHECK_LT(fd_, 0) << "invalid descriptor";
   const int status = close(fd_);
-  // TODO: log status if no zero
-  (void)status;
+  LOG_IF(WARNING, status < 0) << "close() failed with error " << errno;
   fd_ = -1;
 }
 
